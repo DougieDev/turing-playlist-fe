d6b2fd04c1fedee63b2264c0e3aa62880cf78f23
Carlyle Douglas <cdouglascorp@gmail.com>
2020-09-02 10:48:01 -0600

    Adjust formatting and link text

--- src/components/SongCard/SongCard.js
+++ src/components/SongCard/SongCard.js
@@ -1,14 +1,14 @@
 import React from 'react'
+import './SongCard.css'
 
 const SongCard = (props) => {
   return (
     <div className='song-card'>
       <h1 className='song-name'>{props.song}</h1>
       <h2 className='song-artist'>{props.artist}</h2>
-      <a className='song-link'
-      href={props.songLink}>Watch '{props.song}' now!'</a>
+      <a className='song-link' href={props.songLink}>Watch '{props.song}' now!'</a>
     </div>
   )
 }
 
-export default SongCard
+export default SongCard;
